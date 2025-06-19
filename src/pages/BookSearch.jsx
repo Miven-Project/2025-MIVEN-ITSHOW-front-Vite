753ea192f6364b03342826e2595972e0fbbbe249
김가현 <rkgus0324@gmail.com>
2025-06-19 09:14:16 +0900

    fix: naver api 수정

--- src/pages/BookSearch.jsx
+++ src/pages/BookSearch.jsx
@@ -65,8 +65,9 @@ const BookSearch = () => {
   useEffect(() => {
     const fetchBooks = async () => {
       try {
+        const encodedQuery = encodeURIComponent(query);
         const response = await axios.get(`${apiBaseUrl}/api/naver/book?query=`, {
-          params: { query },
+          params: { query: encodedQuery },
         });
         setSearchResults(response.data.items);
       } catch (error) {
